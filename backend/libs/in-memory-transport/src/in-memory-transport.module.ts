9b465025323a9c84e1df4a442f38179bd63edcb3
Mateusz Nowak <kontakt.mateusznowak@gmail.com>
2020-04-07 12:38:03 +0200

    Backend | Delete in-memory transport module.

--- backend/libs/in-memory-transport/src/in-memory-transport.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import {CqrsModule} from "@nestjs/cqrs";
-
-@Module({
-  providers: [CqrsModule],
-  exports: [],
-})
-export class InMemoryTransportModule {}
--- backend/src/write-side/shared-kernel/domain/domain-event.ts
+++ backend/src/write-side/shared-kernel/domain/domain-event.ts
@@ -1,6 +1,11 @@
 import {AggregateId} from './aggregate-id.valueobject';
 import {DomainEventId} from './domain-event-id.valueobject';
 
+/**
+ * Domain event can be used only inside one context - for communication between aggregates and storing state in event sourcing manner.
+ * Cannot be referenced from another module, because changes in those events shouldn't
+ * impact on communication between modules, which may cause cascading changes.
+ */
 export interface DomainEvent<I extends AggregateId = AggregateId, T = any> {
     readonly eventId: DomainEventId;
     readonly occurredAt: Date;
