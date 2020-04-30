0f26642f48d0fc9b037c0a4d00a5d650b16ed39a
KonradSzwarc <konrad.szwarc.dev@gmail.com>
2020-04-30 03:36:25 +0200

    Create front-end boilerplate

---
+++ frontend/craco.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const CracoAntDesignPlugin = require('craco-antd');
+const CracoAlias = require('craco-alias');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAntDesignPlugin,
+    },
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './',
+        tsConfigPath: path.resolve(__dirname, 'tsconfig.paths.json'),
+      },
+    },
+  ],
+  eslint: {
+    enable: false,
+  },
+  typescript: {
+    enableTypeChecking: false,
+  },
+  babel: {
+    plugins: ['emotion'],
+  },
+};
