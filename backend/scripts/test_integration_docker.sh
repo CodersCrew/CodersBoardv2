3e4a22beb4eaa77ab361c11c97f028850b92d505
Mateusz Nowak <kontakt.mateusznowak@gmail.com>
2020-05-14 23:16:01 +0200

    Backend | CB-330 Add docker-compose.test-integration.yml

--- backend/scripts/test_integration_docker.sh
+++ backend/scripts/test_integration_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker-compose -f docker-compose.local.yml up -d  --remove-orphans
+docker-compose -f docker-compose.test-integration.yml up -d  --remove-orphans
 
 jest --config test-integration/jest-integration.json
