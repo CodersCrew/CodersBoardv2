0315012c6695bc6fd54e061f26ebb8514865b284
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2020-05-17 01:57:45 +0200

    add Sider breakpoint

--- frontend/src/components/ant-design/Layout/Layout.stories.tsx
+++ frontend/src/components/ant-design/Layout/Layout.stories.tsx
@@ -73,3 +73,74 @@ export default {
       </Box>
     </Box>
   )
+
+    
+  export const SiderBreakpoint= () => (
+    <Box>
+      <Box  >
+        <Box style={{ textAlignLast: 'center', }}>
+        <h2>Breakpoint</h2>
+        </Box>
+        <Box mt ={30} >
+          <Layout.Layout   style={{ textAlign: 'center' }}>
+            <Layout.Header  height={60} style={{ background: '#7dbceb', }} >Header</Layout.Header>
+            <Layout.Layout >
+              <Layout.Sider  breakpoint ={'xs'} pt={40} style={{  textAlign: 'center',background: '#3ba0e9',}}><p style = {{fontSize : '20px'}}>XS {'<'} 576px</p>Sider</Layout.Sider>
+              <Layout.Content  pt={50} height={120}  style={{ background: '#108ee9', }}>Content</Layout.Content>
+            </Layout.Layout>
+            <Layout.Footer  style={{ background:  '#7dbceb', }}>Footer</Layout.Footer  >
+          </Layout.Layout>
+        </Box>
+        <Box mt ={30}>
+          <Layout.Layout   style={{ textAlign: 'center' }}>
+            <Layout.Header  height={60} style={{ background: '#7dbceb', }} >Header</Layout.Header>
+            <Layout.Layout >
+              <Layout.Sider  breakpoint ={'sm'} pt={40} style={{  textAlign: 'center',background: '#3ba0e9',}}><p style = {{fontSize : '20px'}}>SM {'≥'} 576px</p>Sider</Layout.Sider>
+              <Layout.Content  pt={50} height={120}  style={{ background: '#108ee9', }}>Content</Layout.Content>
+            </Layout.Layout>
+            <Layout.Footer  style={{ background:  '#7dbceb', }}>Footer</Layout.Footer  >
+          </Layout.Layout>
+        </Box>
+        <Box mt ={30}>
+          <Layout.Layout   style={{ textAlign: 'center' }}>
+            <Layout.Header  height={60} style={{ background: '#7dbceb', }} >Header</Layout.Header>
+            <Layout.Layout >
+              <Layout.Sider  breakpoint ={'md'} pt={40} style={{  textAlign: 'center',background: '#3ba0e9',}}><p style = {{fontSize : '20px'}}>MD {'≥'} 768px</p>Sider</Layout.Sider>
+              <Layout.Content  pt={50} height={120}  style={{ background: '#108ee9', }}>Content</Layout.Content>
+            </Layout.Layout>
+            <Layout.Footer  style={{ background:  '#7dbceb', }}>Footer</Layout.Footer  >
+          </Layout.Layout>
+        </Box>
+        <Box mt ={30}>
+          <Layout.Layout   style={{ textAlign: 'center' }}>
+            <Layout.Header  height={60} style={{ background: '#7dbceb', }} >Header</Layout.Header>
+            <Layout.Layout >
+              <Layout.Sider  breakpoint ={'lg'} pt={40} style={{  textAlign: 'center',background: '#3ba0e9',}}><p style = {{fontSize : '20px'}}>LG {'≥'} 992px</p>Sider</Layout.Sider>
+              <Layout.Content  pt={50} height={120}  style={{ background: '#108ee9', }}>Content</Layout.Content>
+            </Layout.Layout>
+            <Layout.Footer  style={{ background:  '#7dbceb', }}>Footer</Layout.Footer  >
+          </Layout.Layout>
+        </Box>
+        <Box mt ={30}>
+          <Layout.Layout   style={{ textAlign: 'center' }}>
+            <Layout.Header  height={60} style={{ background: '#7dbceb', }} >Header</Layout.Header>
+            <Layout.Layout >
+              <Layout.Sider  breakpoint ={'xl'} pt={40} style={{  textAlign: 'center',background: '#3ba0e9',}}><p style = {{fontSize : '20px'}}>XL {'≥'} 1200px</p>Sider</Layout.Sider>
+              <Layout.Content  pt={50} height={120}  style={{ background: '#108ee9', }}>Content</Layout.Content>
+            </Layout.Layout>
+            <Layout.Footer  style={{ background:  '#7dbceb', }}>Footer</Layout.Footer  >
+          </Layout.Layout>
+        </Box>
+        <Box mt ={30}>
+          <Layout.Layout   style={{ textAlign: 'center' }}>
+            <Layout.Header  height={60} style={{ background: '#7dbceb', }} >Header</Layout.Header>
+            <Layout.Layout >
+              <Layout.Sider  breakpoint ={'xxl'} pt={40} style={{  textAlign: 'center',background: '#3ba0e9',}}><p style = {{fontSize : '20px'}}>XXL {'≥'} 1600px</p>Sider</Layout.Sider>
+              <Layout.Content  pt={50} height={120}  style={{ background: '#108ee9', }}>Content</Layout.Content>
+            </Layout.Layout>
+            <Layout.Footer  style={{ background:  '#7dbceb', }}>Footer</Layout.Footer  >
+          </Layout.Layout>
+        </Box>
+      </Box>
+    </Box>
+  )
