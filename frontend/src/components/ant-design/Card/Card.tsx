b80dfff26ba8a89da2c06aabb3bf75178215e030
kevinikos <kevin.nikolas123@gmail.com>
2020-05-17 19:03:41 +0200

    Frontend | CB-317 Storybook stories for different variants of card component

--- frontend/src/components/ant-design/Card/Card.tsx
+++ frontend/src/components/ant-design/Card/Card.tsx
@@ -3,23 +3,30 @@ import { margin, layout, position, padding, display, flexbox, compose } from 'st
 import { Card as AntCard } from 'antd';
 import { CardProps as AntCardProps } from 'antd/lib/card';
 import 'antd/lib/card/style/css';
-import { styled, SpaceProps, LayoutProps, PositionProps, DisplayProps, FlexboxProps } from '@services/theme';
+import {
+  styled,
+  LayoutProps,
+  PositionProps,
+  MarginProps,
+  PaddingProps,
+  DisplayProps,
+  FlexboxProps,
+} from '@services/theme';
 
-export type CardProps = AntCardProps & SpaceProps & LayoutProps & PositionProps;
-export type CardBodyProps = AntCardProps & SpaceProps & DisplayProps & FlexboxProps;
+export type CardProps = AntCardProps & MarginProps & LayoutProps & PositionProps;
+export type CardBodyProps = AntCardProps & PaddingProps & DisplayProps & FlexboxProps;
 
 const StyledCard = styled(AntCard)(margin, layout, position);
 const styledSystem = compose(padding, display, flexbox);
 
 export const Card = (props: CardProps) => <StyledCard {...props} />;
-
 export const CardBody = styled(AntCard)<CardBodyProps>((props) => {
   return {
     border: 'none',
 
     'div.ant-card-body': {
+      padding: 'unset',
       ...styledSystem(props),
-      padding: 0,
     },
   };
 });
