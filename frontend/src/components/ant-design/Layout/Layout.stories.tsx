150f118c36ca07681480803da0c76dc951c80e2f
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2020-05-17 02:02:48 +0200

    add Sider collapsed

--- frontend/src/components/ant-design/Layout/Layout.stories.tsx
+++ frontend/src/components/ant-design/Layout/Layout.stories.tsx
@@ -144,3 +144,40 @@ export default {
       </Box>
     </Box>
   )
+
+
+  export const SiderColapsed= () => (
+    <Box>
+      <Box>
+        <Box style={{ textAlignLast: 'center', }}>
+        <h2>Colapsed true</h2>
+        </Box>
+        <Box mt ={30}>
+          <Layout.Layout  style={{ textAlign: 'center' }}>
+            <Layout.Header  height={60} style={{ background: '#7dbceb', }} >Header</Layout.Header>
+            <Layout.Layout >
+              <Layout.Sider  collapsed ={true}  pt={50} style={{  textAlign: 'center',background: '#3ba0e9', }}>Sider</Layout.Sider>
+              <Layout.Content  pt={50} height={120}  style={{ background: '#108ee9', }}>Content</Layout.Content>
+            </Layout.Layout>
+            <Layout.Footer  style={{ background:  '#7dbceb', }}>Footer</Layout.Footer  >
+          </Layout.Layout>
+        </Box>
+      </Box>
+      <Box>
+        <Box mt ={30} style={{ textAlignLast: 'center', } }>
+        <h2>Colapsed false</h2>
+        </Box>
+        <Box mt ={30}>
+          <Layout.Layout  style={{ textAlign: 'center' }}>
+            <Layout.Header  height={60} style={{ background: '#7dbceb', }} >Header</Layout.Header>
+            <Layout.Layout >
+              <Layout.Sider  collapsed ={false}  pt={50} style={{  textAlign: 'center',background: '#3ba0e9', }}>Sider</Layout.Sider>
+              <Layout.Content  pt={50} height={120}  style={{ background: '#108ee9', }}>Content</Layout.Content>
+            </Layout.Layout>
+            <Layout.Footer  style={{ background:  '#7dbceb', }}>Footer</Layout.Footer  >
+          </Layout.Layout>
+        </Box>
+      </Box>
+     
+    </Box>
+  )
