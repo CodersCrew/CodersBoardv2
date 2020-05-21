013d8af2fc65f31aaeea55869fcb3a3ca0278ae7
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2020-05-21 23:18:02 +0200

    improve code with  eslint clues
    improve code with  eslint clues
    
    improve code with  eslint clues
    
    improve code with  eslint clues
    
    improve code with  eslint clues
    
    improve code with  eslint clues
    
    improve code with  eslint clues

--- frontend/src/components/ant-design/Layout/Layout.tsx
+++ frontend/src/components/ant-design/Layout/Layout.tsx
@@ -1,18 +1,13 @@
-import React, { ComponentProps } from 'react';
-import { compose, space, layout, flexbox  } from 'styled-system';
+import React from 'react';
+import { compose, space, layout, flexbox } from 'styled-system';
 import { Layout as AntLayoute } from 'antd';
 import { LayoutProps as AntLayouteProps, SiderProps } from 'antd/lib/layout';
 import 'antd/lib/layout/style/css';
-import { styled, SpaceProps, LayoutProps, FlexboxProps,} from '@services/theme';
+import { styled, SpaceProps, LayoutProps, FlexboxProps } from '@services/theme';
 
+export type LayoutComponentProps = AntLayouteProps & SpaceProps & LayoutProps & FlexboxProps;
 
-
-
-
-export type LayoutComponentProps = AntLayouteProps & SpaceProps & LayoutProps & FlexboxProps
- 
-export type StyledSiderProps = Omit< SiderProps , 'theme'> &  LayoutComponentProps;
-
+export type StyledSiderProps = Omit<SiderProps, 'theme'> & LayoutComponentProps;
 
 const StyledLayout = styled(AntLayoute)(space, layout, flexbox);
 const StyledLayouteHeader = styled(AntLayoute.Header)(space, layout, flexbox);
@@ -20,22 +15,12 @@ const StyledLayouteContent = styled(AntLayoute.Content)(space, layout, flexbox);
 const StyledLayouteFooter = styled(AntLayoute.Footer)(space, layout, flexbox);
 const styledSystem = compose(space, layout, flexbox);
 
-
-
-export const Layout = (props: LayoutComponentProps) => <StyledLayout {...props} />
-export const Header = (props: LayoutComponentProps) => <StyledLayouteHeader {...props} />
-export const Content = (props: LayoutComponentProps) => <StyledLayouteContent {...props} />
-export const Footer = (props: LayoutComponentProps) => <StyledLayouteFooter {...props} />
+export const Layout = (props: LayoutComponentProps) => <StyledLayout {...props} />;
+export const Header = (props: LayoutComponentProps) => <StyledLayouteHeader {...props} />;
+export const Content = (props: LayoutComponentProps) => <StyledLayouteContent {...props} />;
+export const Footer = (props: LayoutComponentProps) => <StyledLayouteFooter {...props} />;
 export const Sider = styled(AntLayoute.Sider)<StyledSiderProps>((props) => {
-    return {
-        ...styledSystem(props),
-    }
-})
-
-
-
-
-
-
-
-
+  return {
+    ...styledSystem(props),
+  };
+});
